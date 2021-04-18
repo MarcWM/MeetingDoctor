33a808a0917a9b08b7222ef903c6f7b3874561c8
Marc Widmer <marc.widmer.montragull@gmail.com>
2021-04-18 18:14:12 +0200

    Chip Component created

--- app/src/main/java/com/android/meetingdoctors/presentation/components/ChipComponent.kt
+++ app/src/main/java/com/android/meetingdoctors/presentation/components/ChipComponent.kt
@@ -1,2 +1,45 @@
 package com.android.meetingdoctors.presentation.components
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.android.meetingdoctors.data.model.WordOrder
+
+@Composable
+fun ChipComponent(
+    order: WordOrder,
+    isSelected: Boolean,
+    onSelectedOrderChanged: (String) -> Unit,
+    onExecuteSearch: () -> Unit
+) {
+    Surface(
+        modifier = Modifier.padding(end = 8.dp),
+        elevation = 8.dp,
+        shape = MaterialTheme.shapes.medium,
+        color = if(isSelected) Color.LightGray else MaterialTheme.colors.primary
+    ) {
+        Row(modifier = Modifier
+            .toggleable(
+                value = isSelected,
+                onValueChange = {
+                    onSelectedOrderChanged(order.value)
+                    onExecuteSearch()
+                }
+            )
+        ) {
+            Text(
+                text = order.value,
+                style = MaterialTheme.typography.body2,
+                color = Color.White,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
