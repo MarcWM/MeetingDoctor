5c31e614ad74ec284fd5321c5192184c23875f8e
Marc Widmer <marc.widmer.montragull@gmail.com>
2021-04-21 16:30:10 +0200

    Main Activity imports deleted

--- app/src/main/java/com/android/meetingdoctors/presentation/MainActivity.kt
+++ app/src/main/java/com/android/meetingdoctors/presentation/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.android.meetingdoctors.presentation
 
 import android.os.Bundle
-import android.view.View
-import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.ViewTreeLifecycleOwner
 import com.android.meetingdoctors.R
 import dagger.hilt.android.AndroidEntryPoint
 
