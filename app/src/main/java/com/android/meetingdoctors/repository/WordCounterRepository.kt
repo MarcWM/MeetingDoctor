11f14a9c3e794795c1d13d68f090482918190513
Marc Widmer <marc.widmer.montragull@gmail.com>
2021-04-21 16:43:51 +0200

    Repository updated, commented and get list of words updated

--- app/src/main/java/com/android/meetingdoctors/repository/WordCounterRepository.kt
+++ app/src/main/java/com/android/meetingdoctors/repository/WordCounterRepository.kt
@@ -4,14 +4,26 @@ import com.android.meetingdoctors.dataSource.model.Word
 import com.android.meetingdoctors.repository.state.DataState
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Word counter repository
+ */
 interface WordCounterRepository {
 
-    suspend fun getListOfWords(): Flow<DataState<List<Word>>>
+    /**
+     * Get list of words with a limit
+     * @param size: Limit of words to show
+     */
+    suspend fun getListOfWords(size: Int): Flow<DataState<List<Word>>>
 
+    /**
+     * Insert words from a given file
+     * @param fileName: Name of the file
+     */
     suspend fun insertWordsFromNewFile(fileName: String): Flow<DataState<Boolean>>
 
+    /**
+     * Search for string that match with the given query
+     * @param query: String to search
+     */
     suspend fun search(query: String): Flow<DataState<List<Word>>>
-
-    // suspend fun getWord(word: String): Word
-
 }
--- app/src/main/java/com/android/meetingdoctors/repository/WordCounterRepositoryImpl.kt
+++ app/src/main/java/com/android/meetingdoctors/repository/WordCounterRepositoryImpl.kt
@@ -3,12 +3,13 @@ package com.android.meetingdoctors.repository
 import com.android.meetingdoctors.dataSource.cache.CacheDataSource
 import com.android.meetingdoctors.dataSource.fileManager.FileManager
 import com.android.meetingdoctors.dataSource.model.Word
-import com.android.meetingdoctors.dataSource.model.WordEntity
 import com.android.meetingdoctors.repository.state.DataState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import java.lang.Exception
 
+/**
+ * Word Counter repository Implementation
+ */
 class WordCounterRepositoryImpl
 
 constructor(
@@ -16,9 +17,9 @@ constructor(
     private val wordDataSource: CacheDataSource
 ) : WordCounterRepository {
 
-    override suspend fun getListOfWords(): Flow<DataState<List<Word>>> = flow {
+    override suspend fun getListOfWords(size: Int): Flow<DataState<List<Word>>> = flow {
         emit(DataState.Loading)
-        val wordsList =  wordDataSource.getWordList()
+        val wordsList =  wordDataSource.getWordList(size)
         emit(DataState.Success(wordsList))
     }
 
@@ -33,7 +34,10 @@ constructor(
             }
         }
 
-        val listOfWords = fileManager.readAllFileWords(fileName = fileName)
+        val listOfWords = fileManager.readAllFileWords(
+            fileName = fileName,
+            wordDataSource.getAllWordList()
+        )
         wordDataSource.insertNewWordList(words = listOfWords)
         emit(DataState.Success(true))
     }
