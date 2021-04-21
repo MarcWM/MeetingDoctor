1a2775a4fa6089fb42864ee0e7a896313f4f4751
Marc Widmer <marc.widmer.montragull@gmail.com>
2021-04-21 16:42:59 +0200

    TODO removed from Menu screen and some UI modifications done

--- app/src/main/java/com/android/meetingdoctors/presentation/ui/WordCounterMenuScreen.kt
+++ app/src/main/java/com/android/meetingdoctors/presentation/ui/WordCounterMenuScreen.kt
@@ -1,43 +1,78 @@
 package com.android.meetingdoctors.presentation.ui
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.android.meetingdoctors.dataSource.model.Word
-import com.android.meetingdoctors.dataSource.model.WordEntity
 import com.android.meetingdoctors.presentation.components.MenuCardComponent
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+
 
 @ExperimentalCoroutinesApi
 @Composable
 fun WordMenu(
     loading: Boolean = false,
     listOfWords: List<Word>,
-    onNavigateToRecipeDetailScreen: (Int) -> Unit,
+    uploadFileButtonAvailable: Boolean = true,
     onSelectFileSelected: () -> Unit,
+    onTriggerNextPage: () -> Unit
 ) {
 
     if (loading && listOfWords.isEmpty()) {
-        Text(text = "Loading...")
-        // TODO Loader
-    } else if (listOfWords.isEmpty()) {
-        Button(
-            onClick = { onSelectFileSelected() }) {
-            Text(text = "Select File")
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Loading...")
         }
+
+    } else if (listOfWords.isEmpty()) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "No words found..")
+            Spacer(modifier = Modifier.height(4.dp))
+
+            if (uploadFileButtonAvailable) {
+                Text(text = "Maybe upload a new File?")
+                Spacer(modifier = Modifier.height(8.dp))
+                Button(
+                    onClick = { onSelectFileSelected() }) {
+                    Text(text = "Upload File")
+                }
+            }
+        }
+
     } else {
+
         LazyColumn {
             itemsIndexed(
                 items = listOfWords,
             ) { index, word ->
 
-                MenuCardComponent(word = word) {
-
+                if ((index + 1) >= listOfWords.size && !loading) {
+                    onTriggerNextPage()
                 }
+
+                MenuCardComponent(word = word)
             }
         }
+
     }
 
 }
